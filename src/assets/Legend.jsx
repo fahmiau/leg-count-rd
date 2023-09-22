bc0a92609b32a237df459af527cae526d82be5dc
fahmiau <fauliyaay@gmail.com>
2023-09-22 22:41:58 +0700

    delete minus func

--- src/assets/Legend.jsx
+++ src/assets/Legend.jsx
@@ -7,16 +7,16 @@ const Legend = (props) => {
     setCount(count + 1);
   };
 
-  const minus = () => {
-    setCount(count - 1);
-  }
+  // const minus = () => {
+  //   setCount(count - 1);
+  // }
   return (
 
-    <div className="bg-gray-200 w-28 h-36 flex flex-col justify-center items-center rounded-lg drop-shadow-xl">
-      <h5 onClick={minus} className="font-bold text-sm text-center cursor-pointer mb-2">
+    <div onClick={handleClick} className="bg-gray-200 w-28 h-36 flex flex-col justify-center items-center rounded-lg drop-shadow-xl cursor-pointer">
+      <h5 className="font-bold text-sm text-center mb-2">
         {props.name}
       </h5>
-      <div onClick={handleClick} className="h-20 w-20 cursor-pointer">
+      <div className="h-20 w-20">
         <img className="h-20 w-20 " src={"/dice/"+props.link} alt="" />
       </div>
       <h5 className="font-bold text-lg text-blue-800">{count}</h5>
