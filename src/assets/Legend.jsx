269d1da6f7b0208d096e1c0e0b3b98bf5448c22f
fahmiau <fauliyaay@gmail.com>
2023-11-04 23:57:16 +0700

    fix phone responsive

--- src/assets/Legend.jsx
+++ src/assets/Legend.jsx
@@ -13,11 +13,11 @@ const Legend = (props) => {
   return (
 
     <div className="bg-gray-200 h-36 flex flex-col justify-center items-center rounded-lg drop-shadow-xl">
-      <h5 onClick={()=>{substraction();props.sub()}} className="font-bold text-sm text-center mb-2 cursor-pointer hover:text-red-500">
+      <h5 onClick={()=>{substraction();props.sub()}} className="font-bold text-sm text-center mb-2 cursor-pointer hover:text-red-500 max-sm:">
         {props.name}
       </h5>
-      <div onClick={()=>{addition();props.add()}} className="h-20 w-20 cursor-pointer transtition hover:-translate-y-1 hover:scale-110 ">
-        <img className="h-20 w-20 " src={"/dice/"+props.link} alt="" />
+      <div onClick={()=>{addition();props.add()}} className="max-sm:h-14 max-sm:w-14 h-20 w-20 cursor-pointer transtition hover:-translate-y-1 hover:scale-110 ">
+        <img className="h-20 w-20 max-sm:h-14 max-sm:w-14" src={"/dice/"+props.link} alt="" />
       </div>
       <h5 className={'font-bold text-lg '+ ((count == 0) ? 'text-red-500' : 'text-blue-700')}>{count}</h5>
       {/* <button className='border border-black h-1 bg-red-600 w-4' onClick={minus}>-</button> */}
